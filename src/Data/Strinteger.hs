d697dbc9a94f4755ddf6ce984cbd62a9e944e130
Vojtech Mraz <mraz.vojta@gmail.com>
2018-03-28 20:11:24 +0200

    still not working different logic

--- src/Data/Strinteger.hs
+++ src/Data/Strinteger.hs
@@ -2,7 +2,7 @@ module Data.Strinteger where
 
 import Data.Maybe
 -- You might need to use intercalate and splitOn (similar to words/unwords)
-import Data.List (intercalate)
+import Data.List (intercalate, elemIndex)
 import Data.List.Split (splitOn)
 
 -- Use Data.Strinteger.Helpers submodule
@@ -40,12 +40,12 @@ integer2EngNumeral 0 = Just SH.zero
 integer2EngNumeral n 
     | abs n > SH.highestPossible = Nothing
     | n > 0 = Just $ finalString
-    | otherwise = Just $ (SH.negativePrefix ++ [SH.separator] ++ finalString)
+    | otherwise = Just $ (SH.negativePrefix ++ SH.separator ++ finalString)
     where 
-        finalString = intercalate [SH.separator] (scalesCompute (abs n) (reverse SH.scales))
+        finalString = intercalate SH.separator (scalesCompute (abs n) (reverse SH.scales))
         scalesCompute :: Integer -> [(Integer, String)] -> [String]
         scalesCompute 0 _ = []
-        scalesCompute n [] = [intercalate [SH.separatorTens] (tensCompute n (reverse SH.tens))]
+        scalesCompute n [] = [intercalate SH.separatorTens (tensCompute n (reverse SH.tens))]
             where 
                 tensCompute :: Integer -> [(Integer, String)] -> [String]
                 tensCompute 0 _ = []
@@ -57,10 +57,12 @@ integer2EngNumeral n
                         unitsCompute n ((f,s):xs) 
                             | n - f == 0 = [s]
                             | otherwise = unitsCompute n xs
+                
                 tensCompute n ((f,s):xs) 
                     | div n (10 * f) > 1 = tensCompute (div n (10 * f)) xs ++ [s] ++ tensCompute (mod n (10 * f)) xs
                     | div n (10 * f) == 1 = [s] ++ tensCompute (mod n (10 * f)) xs
                     | otherwise = tensCompute (mod n (10 * f)) xs
+        
         scalesCompute n ((f,s):xs) 
             | div n (10 ^ f) > 0 = scalesCompute (div n (10 ^ f)) xs ++ [s] ++ scalesCompute (mod n (10 ^ f)) xs
             | otherwise = scalesCompute (mod n (10 ^ f)) xs 
@@ -68,39 +70,40 @@ integer2EngNumeral n
 -- | Translate String to Integer (if possible)
 -- TODO: implement String->Integer translation
 engNumeral2Integer :: String -> Maybe Integer
-engNumeral2Integer str = case stringSpaces (splitOn [SH.separator] str) of
+engNumeral2Integer str = case stringScales (splitOn SH.separator str) (reverse SH.scales) of
     Just n -> Just n
     _ -> Nothing
     where 
-        stringSpaces :: [String] -> Maybe Integer
-        stringSpaces [] = Nothing
-        stringSpaces [x] = case stringDash (splitOn [SH.separatorTens] x) of
-            Just n -> Just n
-            _ -> Nothing
+        stringScales :: [String] -> [(Integer, String)] -> Maybe Integer
+        stringScales [] _ = Nothing
+        stringScales [n] [] = stringTens (splitOn SH.separatorTens n) (reverse SH.tens)
             where 
-                stringDash :: [String] -> Maybe Integer
-                stringDash [x] = wordExport x
-                stringDash [x,y] = couplesExport x y
-        stringSpaces (x:y:[]) = couplesExport x y
-        stringSpaces (x:y:xs) = restExport (couplesExport x y) xs
-
-        restExport :: Maybe Integer -> [String] -> Maybe Integer
-        restExport ce xs = case (ce, stringSpaces xs) of
-            (Just val1, Just val2) -> Just (val1 + val2)
-            _ -> Nothing
+                stringTens :: [String] -> [(Integer, String)] -> Maybe Integer
+                stringTens [n] ((f,s):xs) 
+                    | n == s = Just (10 * f)
+                    | otherwise = stringTens [n] xs
+                
+                stringTens [n,m] xs = case (stringTens [n] xs, stringTens [m] xs) of
+                    (Just val1, Just val2) -> Just (val1 + val2)
+                    _ -> Nothing 
+                
+                stringTens n [] = stringUnits n (reverse SH.units)
+                    where
+                        stringUnits :: [String] -> [(Integer, String)] -> Maybe Integer
+                        stringUnits [n] ((f,s):xs) 
+                            | n == s = Just f
+                            | otherwise = stringUnits [n] xs
+                        stringUnits _ _ = Nothing
+                
+                stringTens _ _ = Nothing
         
-        couplesExport :: String -> String -> Maybe Integer
-        couplesExport x y = case (stringSpaces [x], wordExport y) of
-            (Just val1, Just val2) -> Just (val1 + val2)
-            _ -> Nothing
-
-        wordExport :: String -> Maybe Integer
-        wordExport str = case SH.word2num str of
-            Just (1, v) -> Just v
-            Just (10, v) -> Just (10 * v)
-            Just (s, 0) -> Just (10 ^ s) 
-            _ -> Nothing
-        
+        stringScales _ [] = Nothing
+        stringScales n ((f,s):xs) = case elemIndex s n of
+            Just i -> case (stringScales (take (i - 1) n) xs, stringScales (drop i n) xs) of
+                (Just val1, Just val2) -> Just (val1 * (10 ^ f) + val2)
+                (Just val1, _) -> Just (val1 * (10 ^ f))
+                _ -> Nothing
+            _ -> stringScales n xs       
 
 -- TODO: implement Strinteger instances of Num, Ord, Eq, Enum, Real, and Integral
 instance Eq Strinteger where
